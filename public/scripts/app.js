fd5a0c4f6cb2b19ac14ffcbee5ea8ec3bdf98798
yousoumar <oumaiyr@gmail.com>
2021-10-01 22:12:19 +0200

    handles images with multer

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,11 @@
+const form = document.querySelector("form");
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 
-button.onclick = () => {
+button.onclick = (e) => {
+  e.preventDefault();
   input.click();
 };
 input.onchange = () => {
-  fetch("/");
+  form.submit();
 };
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const morgan = require("morgan");
+const multer = require("multer");
+
 const app = express();
-
-// set the view engine to ejs
-app.set("view engine", "ejs");
-
-// set public folder
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(morgan("dev"));
+app.use(express.json());
+
+//define storage for the images
+const storage = multer.diskStorage({
+  //destination for files
+  destination: function (request, file, callback) {
+    callback(null, "./public/images");
+  },
+
+  //add back the extension
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+//upload parameters for multer
+const upload = multer({
+  storage: storage,
+});
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/", upload.single("image"), (req, res) => {
+  if (req.file) {
+    console.log(req.file);
+    res.locals.path = "/images/" + req.file.filename;
+    res.render("index");
+  } else throw "error";
 });
 
 app.listen(3000, () => {
